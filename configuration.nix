9385d227a741decc444d9e07caecb85a42d7e15c
Jacob Mussi <jitmussi@gmail.com>
2022-12-17 20:55:28 -0500

    Added Pop-Shell

--- configuration.nix
+++ configuration.nix
@@ -125,8 +125,8 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
   git
-  neovim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
+  neovim 
+  pkgs.gnomeExtensions.pop-shell
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
