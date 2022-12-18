ae77ee061cd553bef3f511e38366b43ede5b9602
Jacob Mussi <jitmussi@gmail.com>
2022-12-17 20:26:07 -0500

    added nvim

--- configuration.nix
+++ configuration.nix
@@ -132,7 +132,7 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
   git
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  nvim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
   ];
 
